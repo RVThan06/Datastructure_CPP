9987fd949ace3b0e92c9ca1bd1912baf53be5ddf
RVThan06 <arvinthevar06@gmail.com>
2026-03-07 16:11:53 +0800

    Update queue comments

--- include/datastructs/queue.hpp
+++ include/datastructs/queue.hpp
@@ -2,22 +2,26 @@
 #define QUEUE_HPP
 
 #include <algorithm>
+#include <concepts>
+#include <cstddef>
+#include <datastructs/utilities.hpp>
 #include <iostream>
 #include <memory>
 #include <stdexcept>
 
 namespace datastructs {
-template <typename T>
-class Queue {
-private:
-    int m_head;
-    int m_tail;
-    int m_capacity;
-    std::unique_ptr<T[]> m_queue;
 
+/**
+ * @brief A templated queue data structure.
+ * * This class implements queue data structure that uses first-in first-out mechanism.
+ * @tparam T is restricted to arithmetic types (integral & floating-point).
+ */
+
+template <datastructs::arithmetic T>
+class Queue {
 public:
-    Queue(int);
-    ~Queue();
+    Queue(std::size_t);
+    ~Queue() {};
     Queue(const Queue&);
     Queue& operator=(const Queue&);
     Queue(Queue&&) noexcept;
@@ -35,34 +39,48 @@ public:
             int temp_head = queue.m_head;
             while (temp_head != queue.m_tail) {
                 out << queue.m_queue[static_cast<std::size_t>(temp_head)] << ", ";
-                temp_head = (temp_head + 1) % queue.m_capacity;
+                temp_head = (temp_head + 1) % static_cast<int>(queue.m_capacity);
             }
             out << queue.m_queue[static_cast<std::size_t>(temp_head)];
         }
         return out;
     }
+
+private:
+    int m_head;
+    int m_tail;
+    std::size_t m_capacity;
+    std::unique_ptr<T[]> m_queue;
 };
 
-// default constructor
-template <typename T>
-Queue<T>::Queue(int capacity) : m_head{-1}, m_tail{-1}, m_capacity{capacity}, m_queue{new T[m_capacity]} {}
+/**
+ * @brief Constructor to initialise queue object.
+ * @param size is the size of queue.
+ */
+template <datastructs::arithmetic T>
+Queue<T>::Queue(std::size_t capacity)
+    : m_head{-1}, m_tail{-1}, m_capacity{capacity}, m_queue{std::make_unique<T[]>(m_capacity)} {}
 
-// destructor
-template <typename T>
-Queue<T>::~Queue() {}
-
-// copy constructor
-template <typename T>
+/**
+ * @brief Copy constructor.
+ * Deep copying is done.
+ * @param other_queue queue object to copy from.
+ */
+template <datastructs::arithmetic T>
 Queue<T>::Queue(const Queue& other_queue)
     : m_head{other_queue.m_head},
       m_tail{other_queue.m_tail},
       m_capacity{other_queue.m_capacity},
-      m_queue{new T[m_capacity]} {
+      m_queue{std::make_unique<T[]>(m_capacity)} {
     std::copy_n(other_queue.m_queue.get(), m_capacity, m_queue.get());
 }
 
-// copy assignment
-template <typename T>
+/**
+ * @brief Copy assignment.
+ * Deep copying is done.
+ * @param other_queue queue object to copy from.
+ */
+template <datastructs::arithmetic T>
 Queue<T>& Queue<T>::operator=(const Queue& other_queue) {
     // self assignment check
     if (this == &other_queue) {
@@ -82,8 +100,12 @@ Queue<T>& Queue<T>::operator=(const Queue& other_queue) {
     return *this;
 }
 
-// move constructor
-template <typename T>
+/**
+ * @brief Move constructor.
+ * * Moves resources from onether queue object, leaves the moved object in valid state.
+ * @param other_queue queue object to move resource from.
+ */
+template <datastructs::arithmetic T>
 Queue<T>::Queue(Queue&& other_queue) noexcept
     : m_head{other_queue.m_head},
       m_tail{other_queue.m_tail},
@@ -95,8 +117,12 @@ Queue<T>::Queue(Queue&& other_queue) noexcept
     other_queue.m_tail = -1;
 }
 
-// move assignment
-template <typename T>
+/**
+ * @brief Move assignment.
+ * * Moves resources from another queue object, leaves the moved object in valid state.
+ * @param other_queue queue object to move resource from.
+ */
+template <datastructs::arithmetic T>
 Queue<T>& Queue<T>::operator=(Queue&& other_queue) noexcept {
     // self assignment check
     if (this == &other_queue) {
@@ -119,24 +145,35 @@ Queue<T>& Queue<T>::operator=(Queue&& other_queue) noexcept {
     return *this;
 }
 
-template <typename T>
+/**
+ * @brief To add data to queue.
+ * * Addition of data happens at the end of the queue.
+ * @throws std::length_error, when trying to add data to queue that is full.
+ */
+template <datastructs::arithmetic T>
 void Queue<T>::enqueue(T value) {
     if (IsEmpty()) {
         ++m_head;
         ++m_tail;
         m_queue[static_cast<std::size_t>(m_tail)] = value;
-    } else if (m_head == (m_tail + 1) % m_capacity) {
-        throw std::length_error("Queue is full");
+    } else if (m_head == (m_tail + 1) % static_cast<int>(m_capacity)) {
+        throw std::length_error("Queue is full.");
     } else {
-        m_tail = (m_tail + 1) % m_capacity;
+        m_tail = (m_tail + 1) % static_cast<int>(m_capacity);
         m_queue[static_cast<std::size_t>(m_tail)] = value;
     }
 }
 
-template <typename T>
+/**
+ * @brief To remove data from queue.
+ * * Removal of data happens at the front of the queue.
+ * @return copy of data removed from front of queue.
+ * @throws std::length_error, when trying to remove data from empty queue.
+ */
+template <datastructs::arithmetic T>
 T Queue<T>::dequeue() {
     if (IsEmpty()) {
-        throw std::length_error("Queue is full");
+        throw std::length_error("Queue is empty.");
     } else if (m_head == m_tail) {
         T value = m_queue[static_cast<std::size_t>(m_head)];
         m_head = -1;
@@ -144,21 +181,30 @@ T Queue<T>::dequeue() {
         return value;
     } else {
         T value = m_queue[static_cast<std::size_t>(m_head)];
-        m_head = (m_head + 1) % m_capacity;
+        m_head = (m_head + 1) % static_cast<int>(m_capacity);
         return value;
     }
 }
 
-template <typename T>
+/**
+ * @brief To peak data at the front of the queue.
+ * @throws std::length_error, when trying to peak from empty queue.
+ * @return a copy of data at the front of the queue.
+ */
+template <datastructs::arithmetic T>
 T Queue<T>::front() const {
     if (IsEmpty()) {
-        throw std::length_error("Queue is full");
+        throw std::length_error("Queue is empty.");
     } else {
         return m_queue[static_cast<std::size_t>(m_head)];
     }
 }
 
-template <typename T>
+/**
+ * @brief To check if the queue is empty.
+ * @return boolean data, true if queue is empty, false if otherwise.
+ */
+template <datastructs::arithmetic T>
 bool Queue<T>::IsEmpty() const {
     if (m_head == -1 && m_tail == -1) {
         return true;
